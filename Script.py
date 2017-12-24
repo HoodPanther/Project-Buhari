5be61022a245d1269c54726ff65a85656a13aaa9
chibueze07 <bueze_2007@yahoo.com>
2017-12-24 19:45:12 +0100

    Second

--- Script.py
+++ Script.py
@@ -14,21 +14,6 @@ import os
 import sys
 
 
-'''
-In order to use this script you should register a data-mining application
-with Twitter.  Good instructions for doing so can be found here:
-http://marcobonzanini.com/2015/03/02/mining-twitter-data-with-python-part-1/
-
-After doing this you can copy and paste your unique consumer key,
-consumer secret, access token, and access secret into the load_api()
-function below.
-
-The main() function can be run by executing the command: 
-python twitter_search.py
-
-I used Python 3 and tweepy version 3.5.0.  You will also need the other
-packages imported above.
-'''
 
 def load_api():
     ''' Function that loads the twitter API after authorizing the user. '''
